385d3aad80ceb833abfe351304c15a0fbf10bf65
Monica Piras <monica.piras.info@gmail.com>
2022-07-18 15:51:52 +0200

    update changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var MOCK_DATA = require("./mockData");
 const init = async () => {
 
     const server = Hapi.server({
-        port:  3000,
+        port:  process.env.PORT || 3000,
         host: 'localhost'
     });
 
