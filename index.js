a26ba671cd50336334a638c18adab7d00824a9bb
Monica Piras <monica.piras.info@gmail.com>
2022-07-18 16:16:45 +0200

    update changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const init = async () => {
 
     const server = Hapi.server({
         port:  process.env.PORT || 3000,
-        host: 'localhost'
+        host: '0.0.0.0'
     });
 
     await server.register({
